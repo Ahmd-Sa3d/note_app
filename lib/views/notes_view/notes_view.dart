59d688461a589f21e67184a9982c7a699f519c30
Ahmed saad <ahmdsaad2002@gmail.com>
2026-05-31 18:06:55 +0300

    add modal bottom sheet add border radius to the modal bottom sheet

--- lib/views/notes_view/notes_view.dart
+++ lib/views/notes_view/notes_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/core/utils/colors.dart';
+import 'package:note_app/views/notes_view/widget/add_bottom_sheet.dart';
 import 'package:note_app/views/notes_view/widget/app_bar_component/Notes_View_app_bar.dart';
 import 'package:note_app/views/notes_view/widget/notes_list_view.dart';
 
@@ -9,8 +10,21 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: IconButton(onPressed: () {},
-        icon: Icon(Icons.add,color: AppColors.black,size: 35,),style: IconButton.styleFrom(backgroundColor: AppColors.teal),),
+      floatingActionButton: IconButton(
+        onPressed: () {
+          showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadiusGeometry.circular(16),
+            ),
+            context: context,
+            builder: (context) {
+              return AddBottomSheet();
+            },
+          );
+        },
+        icon: Icon(Icons.add, color: AppColors.black, size: 35),
+        style: IconButton.styleFrom(backgroundColor: AppColors.teal),
+      ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
