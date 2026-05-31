c65a973b61fed4c29fbf174c9cab7cae751f886a
Ahmed saad <ahmdsaad2002@gmail.com>
2026-05-31 09:58:25 +0300

    add notes_list_view.dart file

--- lib/views/notes_view/notes_view.dart
+++ lib/views/notes_view/notes_view.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:note_app/views/notes_view/widget/app_bar_component/Notes_View_app_bar.dart';
-import 'package:note_app/views/notes_view/widget/notes_view_items.dart';
+import 'package:note_app/views/notes_view/widget/notes_list_view.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
@@ -13,7 +12,8 @@ class NotesView extends StatelessWidget {
       child: Column(
         children: [
           NotesViewAppBar(),
-          NotesViewItems(),
+          SizedBox(height: 30,),
+          NotesListView(),
         ],
       ),
     )));
---
+++ lib/views/notes_view/widget/notes_list_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:note_app/views/notes_view/widget/notes_view_items.dart';
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: 2,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding:  EdgeInsets.symmetric(vertical: 15.0),
+            child: NotesViewItems(),
+          );
+        },
+      ),
+    );
+  }
+}
