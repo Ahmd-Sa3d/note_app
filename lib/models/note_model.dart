6811d6c26404b0fa2dfda2f72b3fc61e86dbf604
Ahmed saad <ahmdsaad2002@gmail.com>
2026-06-09 07:23:15 +0300

    add hive field

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,10 +1,15 @@
 import 'package:hive/hive.dart';
+part 'note_model.g.dart';
 @HiveType(typeId: 0)
 class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
-  final String date;
-  final int color;
+  @HiveField(2)
+    final String date;
+  @HiveField(3)
+    final int color;
 
   NoteModel({
     required this.title,
