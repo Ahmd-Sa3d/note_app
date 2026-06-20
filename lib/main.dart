2b14cbc2b82aa932bc28fdb2305baa8c63dff2d8
Ahmed saad <ahmdsaad2002@gmail.com>
2026-06-20 08:19:39 +0300

    provide add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/models/note_model.dart';
+import 'package:note_app/services/cubits/add_notes_cubit.dart';
 import 'package:note_app/views/edit_note_view.dart';
 import 'package:note_app/views/notes_view/notes_view.dart';
 
-void main()async {
+void main() async {
   await Hive.initFlutter();
   await Hive.openBox('kHiveBox');
   Hive.registerAdapter(NoteModelAdapter());
@@ -20,15 +23,17 @@ class NotesApp extends StatelessWidget {
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, child) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(brightness: Brightness.dark),
-        initialRoute: NotesView().noteId,
-        routes: {
-          NotesView().noteId: (context) => const NotesView(),
-          EditNoteView().noteId : (context)=> const EditNoteView()
-        },
-
+      builder: (context, child) => MultiBlocProvider(
+        providers: [BlocProvider(create: (context) => AddNotesCubit())],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(brightness: Brightness.dark),
+          initialRoute: NotesView().noteId,
+          routes: {
+            NotesView().noteId: (context) => const NotesView(),
+            EditNoteView().noteId: (context) => const EditNoteView(),
+          },
+        ),
       ),
     );
   }
